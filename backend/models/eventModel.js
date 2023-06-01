1f8242f922eb3fab2edfb4a07a2fa652f8bf70b6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-06-01 22:11:02 +0530

    added event model

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -1,5 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({})
+const eventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    tagline: {
+        type: String,
+        required: true
+    },
+    schedule: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    files: [{
+        type: String,
+        required: true
+    }],
+    moderator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    sub_category: {
+        type: String,
+        required: true
+    },
+    rigor_rank: {
+        type: Number,
+        required: true
+    },
+    attendees: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
 
 module.exports = mongoose.model('Event', eventSchema)

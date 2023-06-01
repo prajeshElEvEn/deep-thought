96544af59641ca66cfd8862bfc70778b7367f0c6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-06-01 21:33:59 +0530

    written server script

---
+++ backend/models/eventModel.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({})
+
+module.exports = mongoose.model('Event', eventSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
+
+const app = express()
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
